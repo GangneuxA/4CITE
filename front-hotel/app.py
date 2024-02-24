177ca15185c029bf8ef0475005bbb5b137ea1772
GangneuxA <gangneux.alex@gmail.com>
2024-02-24 15:56:20 +0100

    Commit for login/register templates

--- front-hotel/app.py
+++ front-hotel/app.py
@@ -4,6 +4,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+@app.route('/home')
+def home():
+    return render_template('home.html', utc_dt=datetime.datetime.utcnow())
+
+@app.route('/login')
+def login():
+    return render_template('login.html', utc_dt=datetime.datetime.utcnow())
+
+@app.route('/register')
+def register():
+    return render_template('register.html', utc_dt=datetime.datetime.utcnow())
