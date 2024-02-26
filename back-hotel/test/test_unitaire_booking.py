cf1c712e0040801556d45eb11dc0bdb1e8bbde3e
Julien boisgard <julien.boisgard37@gmail.com>
2024-02-26 20:13:51 +0100

    fix bug

--- back-hotel/test/test_unitaire_booking.py
+++ back-hotel/test/test_unitaire_booking.py
@@ -1,6 +1,6 @@
 import unittest
 from app.routes import app ,db
-from app.models import booking
+from app.models import booking,user,chambres,hotel
 
 class TestbookingGet(unittest.TestCase):
     def setUp(self):
@@ -8,6 +8,105 @@ class TestbookingGet(unittest.TestCase):
         self.app_context = app.app_context()
         self.app_context.push()
         self.client = app.test_client()
+        self.admin_token = self.create_admin_user(name="admin", mail="admin@example.com", role="admin")
+        self.user_token = self.create_admin_user(name="user", mail="user@example.com", role="user")
+        self.hotel_obj = hotel(name="Hotel ABC", location="City XYZ", description="A luxurious hotel")
+        db.session.add(hotel_obj)
+        db.session.commit()
+
+        self.chambre1 = chambres(numero="101", nb_personne=2, hotel_id=self.hotel_obj.id)
+        self.chambre2 = chambres(numero="102", nb_personne=2, hotel_id=self.hotel_obj.id)
+        db.session.add_all([self.chambre1, self.chambre2])
+        db.session.commit()
+
+    def tearDown(self):
+        admin_user = user.query.filter_by(email="admin@example.com").first()
+        if admin_user:
+            db.session.delete(admin_user)
+            db.session.commit()
+
+        user_user = user.query.filter_by(email="user@example.com").first()
+        if user_user:
+            db.session.delete(user_user)
+            db.session.commit()
+        db.session.delete(self.chambre1)
+        db.session.delete(self.chambre2)
+        db.session.delete(self.hotel_obj)
+        db.session.commit()
+        
+    def test_get_booking_as_admin(self):
+        with self.client:
+            user1 = user.query.filter_by(email="user@example.com").first()
+            user2 = user(ename="user",mail="user2@example.com", role="user")
+            user2.set_password('password')
+            db.session.add(user2)
+            db.session.commit()
+            booking1 = booking(user_id=user1.id, room_id=1, start_date="2024-03-01", end_date="2024-03-05")
+            booking2 = booking(user_id=user2.id, room_id=2, start_date="2024-03-10", end_date="2024-03-15")
+            db.session.add_all([booking1, booking2])
+            db.session.commit()
+            response = self.client.get('/booking', headers={'Authorization': f'Bearer {self.admin_token}'})
+            data = response.get_json()
+            
+            db.session.delete(booking1)
+            db.session.delete(booking2)
+            db.session.delete(user2)
+            db.session.commit()
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIsInstance(data, list)
+            self.assertGreaterEqual(len(data), 2)
+
+    def test_get_booking_for_specific_user_as_admin(self):
+        with self.client:
+            user = user.query.filter_by(email="user@example.com").first()
+            db.session.add(user)
+            db.session.commit()
+            booking1 = booking(user_id=user.id, room_id=1, start_date="2024-03-01", end_date="2024-03-05")
+            booking2 = booking(user_id=user.id, room_id=2, start_date="2024-03-10", end_date="2024-03-15")
+            db.session.add_all([booking1, booking2])
+            db.session.commit()
+            response = self.client.get(f'/booking?email={user.email}', headers={'Authorization': f'Bearer {self.admin_token}'})
+            data = response.get_json()
+            
+            db.session.delete(booking1)
+            db.session.delete(booking2)
+            db.session.commit()
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIsInstance(data, list)
+            self.assertEqual(len(data), 2)
+
+    def test_get_booking_as_user(self):
+        with self.client:
+            user = user.query.filter_by(email="user@example.com").first()
+            db.session.add(user)
+            db.session.commit()
+            booking1 = booking(user_id=user.id, room_id=1, start_date="2024-03-01", end_date="2024-03-05")
+            booking2 = booking(user_id=user.id, room_id=2, start_date="2024-03-10", end_date="2024-03-15")
+            db.session.add_all([booking1, booking2])
+            db.session.commit()
+            response = self.client.get('/booking', headers={'Authorization': f'Bearer {self.user_token}'})
+            data = response.get_json()
+
+            db.session.delete(booking1)
+            db.session.delete(booking2)
+            db.session.commit()
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIsInstance(data, list)
+            self.assertEqual(len(data), 2)
+
+
+    def create_admin_user(self, name , mail , role):
+        admin_user = user(pseudo=name, email=mail, role=role)
+        admin_user.set_password("admin_password")
+        db.session.add(admin_user)
+        db.session.commit()
+        response = self.client.post('/login', json={'email': mail, 'password': 'admin_password'})
+        data = response.get_json()
+        return data['access_token']
+
 
 if __name__ == '__main__':
     unittest.main()
--- back-hotel/test/test_unitaire_chambres.py
+++ back-hotel/test/test_unitaire_chambres.py
@@ -67,7 +67,215 @@ class TestChambresGet(unittest.TestCase):
 
             self.assertIsInstance(data, list)
             self.assertEqual(len(data), 0)
+
+class TestChambresPost(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app_context = app.app_context()
+        self.app_context.push()
+        self.client = app.test_client()
+        self.admin_token = self.create_admin_user(name="admin", mail="admin@example.com", role="admin")
+        self.user_token = self.create_admin_user(name="user", mail="user@example.com", role="user")
     
+    def tearDown(self):
+        admin_user = user.query.filter_by(email="admin@example.com").first()
+        if admin_user:
+            db.session.delete(admin_user)
+            db.session.commit()
+
+        user_user = user.query.filter_by(email="user@example.com").first()
+        if user_user:
+            db.session.delete(user_user)
+            db.session.commit()
+
+    def test_create_chambres_as_admin(self):
+        with self.client:
+            hotel_obj = hotel(name="Hotel ABC", location="City XYZ", description="A luxurious hotel")
+            db.session.add(hotel_obj)
+            db.session.commit()
+
+            chambre_body= {"numero": "101", "nb_personne": 2, "hotel_id": hotel_obj.id}
+
+            response = self.client.post('/chambres', json=chambre_body, headers={'Authorization': f'Bearer {self.admin_token}'})
+            data = response.get_json()
+            chambres_obj = chambres.query.get(data["id"])
+            db.session.delete(chambres_obj)
+            db.session.delete(hotel_obj)
+            db.session.commit()
+            
+            self.assertEqual(response.status_code, 201)
+            self.assertIsInstance(data, dict)
+            self.assertEqual(data['numero'], '101')
+
+    def test_create_chambres_missing_json(self):
+        with self.client:
+            response = self.client.post('/chambres', json={},headers={'Authorization': f'Bearer {self.admin_token}'})
+            data = response.get_json()
+
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('error', data)
+            self.assertEqual(data['error'], 'json not found')
+
+    def test_create_chambres_unauthorized(self):
+        with self.client:
+            chambre_body = {"numero": "101", "nb_personne": 2, "hotel_id": 1}
+
+            response = self.client.post('/chambres', json=chambre_body, headers={'Authorization': f'Bearer {self.user_token}'})
+            data = response.get_json()
+
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('error', data)
+            self.assertEqual(data['error'], 'You have not the permission for this')
+
+    def create_admin_user(self, name , mail , role):
+        admin_user = user(pseudo=name, email=mail, role=role)
+        admin_user.set_password("admin_password")
+        db.session.add(admin_user)
+        db.session.commit()
+        response = self.client.post('/login', json={'email': mail, 'password': 'admin_password'})
+        data = response.get_json()
+        return data['access_token']
+
+class TestChambresPut(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app_context = app.app_context()
+        self.app_context.push()
+        self.client = app.test_client()
+        self.admin_token = self.create_admin_user(name="admin", mail="admin@example.com", role="admin")
+        self.user_token = self.create_admin_user(name="user", mail="user@example.com", role="user")
+    
+    def tearDown(self):
+        admin_user = user.query.filter_by(email="admin@example.com").first()
+        if admin_user:
+            db.session.delete(admin_user)
+            db.session.commit()
+
+        user_user = user.query.filter_by(email="user@example.com").first()
+        if user_user:
+            db.session.delete(user_user)
+            db.session.commit()
+
+    def test_put_chambres_as_admin(self):
+        with self.client:
+            hotel_obj = hotel(name="Hotel ABC", location="City XYZ", description="A luxurious hotel")
+            db.session.add(hotel_obj)
+            db.session.commit()
+
+            chambre_body= {"numero": "101", "nb_personne": 2, "hotel_id": hotel_obj.id}
+            response = self.client.post('/chambres', json=chambre_body, headers={'Authorization': f'Bearer {self.admin_token}'})
+            data = response.get_json()
+            
+            chambre_body= {"numero": "102", "nb_personne": 2}
+            response = self.client.put('/chambres/'+str(data["id"]), json=chambre_body, headers={'Authorization': f'Bearer {self.admin_token}'})
+            data = response.get_json()
+
+            chambres_obj = chambres.query.get(data["id"])
+            db.session.delete(chambres_obj)
+            db.session.delete(hotel_obj)
+            db.session.commit()
+            
+            self.assertEqual(response.status_code, 200)
+            self.assertIsInstance(data, dict)
+            self.assertEqual(data['numero'], '102')
+
+    def test_put_chambres_missing_json(self):
+        with self.client:
+            response = self.client.put('/chambres/1', json={},headers={'Authorization': f'Bearer {self.admin_token}'})
+            data = response.get_json()
+
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('error', data)
+            self.assertEqual(data['error'], 'json not found')
+
+    def test_put_chambres_unauthorized(self):
+        with self.client:
+            chambre_body = {"numero": "101", "nb_personne": 2, "hotel_id": 1}
+
+            response = self.client.put('/chambres/2', json=chambre_body, headers={'Authorization': f'Bearer {self.user_token}'})
+            data = response.get_json()
+
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('error', data)
+            self.assertEqual(data['error'], 'You have not the permission for this')
+
+    def create_admin_user(self, name , mail , role):
+        admin_user = user(pseudo=name, email=mail, role=role)
+        admin_user.set_password("admin_password")
+        db.session.add(admin_user)
+        db.session.commit()
+        response = self.client.post('/login', json={'email': mail, 'password': 'admin_password'})
+        data = response.get_json()
+        return data['access_token']
+
+class TestDeleteChambres(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app_context = app.app_context()
+        self.app_context.push()
+        self.client = app.test_client()
+        self.admin_token = self.create_admin_user(name="admin", mail="admin@example.com", role="admin")
+        self.user_token = self.create_admin_user(name="user", mail="user@example.com", role="user")
+    
+    def tearDown(self):
+        admin_user = user.query.filter_by(email="admin@example.com").first()
+        if admin_user:
+            db.session.delete(admin_user)
+            db.session.commit()
+
+        user_user = user.query.filter_by(email="user@example.com").first()
+        if user_user:
+            db.session.delete(user_user)
+            db.session.commit()
+
+    def test_delete_chambres_as_admin(self):
+        with self.client:
+            hotel_obj = hotel(name="Hotel ABC", location="City XYZ", description="A luxurious hotel")
+            db.session.add(hotel_obj)
+            db.session.commit()
+            chambre = chambres(numero="101", nb_personne=2, hotel_id=hotel_obj.id)
+            db.session.add(chambre)
+            db.session.commit()
+
+            response = self.client.delete(f'/chambres/{chambre.id}', headers={'Authorization': f'Bearer {self.admin_token}'})
+            
+            chambre_obj= chambres.query.get(chambre.id)
+            db.session.delete(chambre)
+            db.session.delete(hotel_obj)
+            db.session.commit()
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIsNone(chambre_obj)
+
+    def test_delete_chambres_non_existing(self):
+        with self.client:
+            response = self.client.delete('/chambres/999', headers={'Authorization': f'Bearer {self.admin_token}'})
+            data = response.get_json()
+
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('error', data)
+            self.assertEqual(data['error'], 'hotel not found')
+
+    def test_delete_chambres_unauthorized(self):
+        with self.client:
+            response = self.client.delete(f'/chambres/2', headers={'Authorization': f'Bearer {self.user_token}'})
+            data = response.get_json()
+
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('error', data)
+            self.assertEqual(data['error'], 'You have not the permission for this')
+
+    def create_admin_user(self, name , mail , role):
+        admin_user = user(pseudo=name, email=mail, role=role)
+        admin_user.set_password("admin_password")
+        db.session.add(admin_user)
+        db.session.commit()
+        response = self.client.post('/login', json={'email': mail, 'password': 'admin_password'})
+        data = response.get_json()
+        return data['access_token']
+
+
+
 if __name__ == '__main__':
     unittest.main()
     app.run()
